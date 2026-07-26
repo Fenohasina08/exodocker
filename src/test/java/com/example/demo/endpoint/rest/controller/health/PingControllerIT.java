efc8431c472338ff5c56edab77bc0e7d4a2d3f5f
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-07-27 00:28:06 +0300

    feat(v2sql): delete the dublon from v1

---
+++ src/test/java/com/example/demo/endpoint/rest/controller/health/PingControllerIT.java
@@ -0,0 +1,22 @@
+package com.example.demo.endpoint.rest.controller.health;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.example.demo.conf.FacadeIT;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+
+class PingControllerIT extends FacadeIT {
+
+    @Autowired private TestRestTemplate restTemplate;
+
+    @Test
+    void ping_devrait_repondre_pong() {
+        var response = restTemplate.getForEntity("/ping", String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualTo("pong");
+    }
+}
