c3afd2e55c38a71ca2c8d49a6fb2352039632279
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-07-28 12:06:12 +0300

    test: add Sentry test endpoints

--- src/main/java/com/example/demo/endpoint/rest/controller/ErrorController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/ErrorController.java
@@ -1,22 +1,21 @@
 package com.example.demo.endpoint.rest.controller;
 
-import io.sentry.Sentry;
-import org.springframework.http.ResponseEntity;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class ErrorController {
 
+  @GetMapping("/exception")
+  public void getError() {
+    throw new RuntimeException("This is an error");
+  }
+
   @GetMapping("/log-error")
-  public ResponseEntity<String> logError() {
-
-    try {
-      throw new Exception("This is a test.");
-    } catch (Exception e) {
-      Sentry.captureException(e);
-    }
-
-    return ResponseEntity.ok("Error has been sent to Sentry");
+  public String logError() {
+    log.error("This is an error");
+    return "OK";
   }
 }
