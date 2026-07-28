f4c18076bb2d916c1beb55639a970ef614676df5
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-07-28 22:43:57 +0300

    feat: add logError

--- src/main/java/com/example/demo/endpoint/rest/controller/TestController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.example.demo.endpoint.rest.controller;
 
+import static org.reflections.Reflections.log;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +11,11 @@ public class TestController {
   @GetMapping("/exception")
   public void getError() {
     throw new RuntimeException("This is an error");
+  }
+
+  @GetMapping("/log-error")
+  public String logError() {
+    log.error("This is an error");
+    return "OK";
   }
 }
