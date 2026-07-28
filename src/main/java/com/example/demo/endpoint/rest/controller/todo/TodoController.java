d529e919e0f96d8be517135e06084cc4a374e812
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-07-28 11:07:28 +0300

    feat: add format sh for sentry at todocontroller

--- src/main/java/com/example/demo/endpoint/rest/controller/todo/TodoController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/todo/TodoController.java
@@ -25,7 +25,7 @@ public class TodoController {
 
   @GetMapping
   public ResponseEntity<List<TodoResponse>> getTodos(
-          @RequestParam(name = "onlyCompleted", required = false) Boolean onlyCompleted) {
+      @RequestParam(name = "onlyCompleted", required = false) Boolean onlyCompleted) {
 
     List<Todo> todos;
 
@@ -39,7 +39,7 @@ public class TodoController {
     }
 
     List<TodoResponse> responses =
-            todos.stream().map(todoMapper::toResponse).collect(Collectors.toList());
+        todos.stream().map(todoMapper::toResponse).collect(Collectors.toList());
 
     try {
       throw new Exception("This is a test.");
